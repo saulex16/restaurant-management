c16e4794f56672658b2fed38493fd8f5d2dcdba5
saulex16 <scastaneda@itba.edu.ar>
2025-03-06 23:07:59 +0100

    build: project initialization

---
+++ src/main/java/at/technikum_wien/restaurant_management/RestaurantManagementApplication.java
@@ -0,0 +1,12 @@
+package at.technikum_wien.restaurant_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantManagementApplication.class, args);
+	}
+}
---
+++ src/test/java/at/technikum_wien/restaurant_management/RestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum_wien.restaurant_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
