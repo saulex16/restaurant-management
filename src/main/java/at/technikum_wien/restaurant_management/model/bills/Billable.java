62397c9a1ec4f0adb1bc38bf066a508f869c815e
saulex16 <scastaneda@itba.edu.ar>
2025-04-12 15:54:39 +0200

    build: tp3 project setup

--- src/main/java/at/technikum_wien/restaurant_management/model/bills/Billable.java
+++
@@ -1,5 +0,0 @@
-package at.technikum_wien.restaurant_management.model.bills;
-
-public interface Billable {
-    void accept(BillVisitor visitor);
-}
--- src/main/java/at/technikum_wien/restaurant_management/repository/interfaces/TableRepository.java
+++
@@ -1,7 +0,0 @@
-package at.technikum_wien.restaurant_management.repository.interfaces;
-
-import at.technikum_wien.restaurant_management.model.tables.Table;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TableRepository extends JpaRepository<Table, Long> {
-}
