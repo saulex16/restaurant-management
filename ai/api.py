0762d69f7b0e3b8d201164c6c76159c92531eeea
saulex16 <scastaneda@itba.edu.ar>
2025-04-20 11:11:51 +0200

    fix: ai api imports

--- ai/api.py
+++ ai/api.py
@@ -6,7 +6,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, UploadFile, File, Depends
 
 from models.requests import ChatRequest
-from services.rag_service import RAGService, RAGServiceImpl
+from services.rag_service import RagService
 import utils
 
 # restaurant_management_api_service = utils.create_restaurant_management_api_service()
@@ -22,10 +22,10 @@ import utils
 app = FastAPI()
 
 def get_rag_service():
-    return RAGServiceImpl()
+    return RagService()
 
 @app.post("/upload/")
-async def upload_file(file: UploadFile = File(...), service: RAGService = Depends(get_rag_service)):
+async def upload_file(file: UploadFile = File(...), service: RagService = Depends(get_rag_service)):
     contents = await file.read()
 
     with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
@@ -36,11 +36,11 @@ async def upload_file(file: UploadFile = File(...), service: RAGService = Depend
     return {"message": "Upload successful", "details": result}
 
 @app.post("/chat/")
-async def chat(request: ChatRequest, service: RAGService = Depends(get_rag_service)):
+async def chat(request: ChatRequest, service: RagService = Depends(get_rag_service)):
     answer = service.make_query(request.message)
     return {"answer": answer}
 
 @app.post("/command/{restaurant_id}")
-async def command(restaurant_id: int, service: RAGService = Depends(get_rag_service)):
+async def command(restaurant_id: int, service: RagService = Depends(get_rag_service)):
     answer = await service.send_command(restaurant_id, "listar recetas posibles")
     return {"answer": answer}
